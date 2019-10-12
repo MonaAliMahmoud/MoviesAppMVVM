d772c0969a521ec5aa19ac8d2db93911622855cf
Mona <mona.ali3c@gmail.com>
2019-10-12 20:25:49 +0200

    Fix duplicate list

--- app/src/main/java/com/hemdan/mvvm/ui/main/MainActivity.kt
+++ app/src/main/java/com/hemdan/mvvm/ui/main/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.hemdan.mvvm.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
 import com.hemdan.mvvm.R
 import com.hemdan.mvvm.ui.base.BaseActivity
-import com.hemdan.mvvm.ui.main.actorslist.ActorsListFragment
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fragmentManager: FragmentManager = this.supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.list_fragment, ActorsListFragment())
-        fragmentTransaction.commit()
     }
 }
