a7960a4bf3f465394275a269aa91936f5697d85c
Mona Ali <mona.ali@ibtikar.net.sa>
2019-10-13 16:09:38 +0200

    Add retrofitApi call for pictures

--- app/src/main/java/com/hemdan/mvvm/data/api/RetrofitApi.kt
+++ app/src/main/java/com/hemdan/mvvm/data/api/RetrofitApi.kt
@@ -1,10 +1,12 @@
 package com.hemdan.mvvm.data.api
 
 import com.hemdan.mvvm.data.model.PopularList
+import com.hemdan.mvvm.data.model.PopularProfile
 import com.hemdan.mvvm.di.module.NetworkModule.Companion.POPULAR_PEOPLE_URL
 import com.hemdan.mvvm.di.module.NetworkModule.Companion.SEARCH_URL
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RetrofitApi {
@@ -14,4 +16,7 @@ interface RetrofitApi {
     @GET(SEARCH_URL)
     fun searchForActors(@Query ("query") searchStr: String,
                         @Query ("page") page: Int): Single<PopularList>
+
+    @GET("person/{popularId}/images?")
+    fun getProfiles(@Path("popularId") popularId: String): Single<PopularProfile>
 }
--- app/src/main/java/com/hemdan/mvvm/ui/main/actordetails/ActorDetailsRepository.kt
+++ app/src/main/java/com/hemdan/mvvm/ui/main/actordetails/ActorDetailsRepository.kt
@@ -1,6 +1,17 @@
 package com.hemdan.mvvm.ui.main.actordetails
 
+import com.hemdan.mvvm.data.api.RetrofitApi
+import com.hemdan.mvvm.data.model.PopularProfile
 import com.hemdan.mvvm.ui.base.BaseRepository
+import io.reactivex.Single
+import javax.inject.Inject
 
 class ActorDetailsRepository: BaseRepository() {
+
+    @Inject
+    lateinit var retrofitApi: RetrofitApi
+
+    fun getActorPictures(popularId: String): Single<PopularProfile> {
+        return retrofitApi.getProfiles(popularId)
+    }
 }
